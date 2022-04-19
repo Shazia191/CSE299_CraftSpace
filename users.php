3125e046abe33cc7bee9ae09da6a66f2164e755e
Shazia191 <72934185+Shazia191@users.noreply.github.com>
2022-04-19 12:14:35 +0600

    Add files via upload

--- users.php
+++ users.php
@@ -1,15 +1,42 @@
 <?php
   session_start();
-  include_once "config.php";
-  $outgoing_id = $_SESSION['unique_id'];
-  $sql = mysqli_query($conn, "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id}");
-  $output = "";
-
-  if(mysqli_num_rows($sql) == 1){
-    $output .= "No users are available now!";
-  }elseif(mysqli_num_rows($sql) > 0){
-    include "data.php";
+  if(!isset($_SESSION['unique_id'])){
+   header("location: login.php");
   }
-  echo $output;
-
 ?>
+
+<?php include_once "header.php"; ?> 
+<body>
+  <div class="wrapper">
+   <section class="users">
+     <header>
+       <?php
+        include_once "php/config.php";
+        $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id= {$_SESSION['unique_id']}");
+        if(mysqli_num_rows($sql) > 0){
+          $row = mysqli_fetch_assoc($sql);
+        }
+       ?>
+       <div class="content">
+          <img src="php/images/<?php echo $row['img'] ?>" alt="">
+          <div class="details">
+          <span><?php echo $row['fname'] . " " . $row['lname'] ?></span>
+          <p><?php echo $row['status'] ?></p>
+        </div>
+       </div>
+       <a href="php/logout.php?logout_id=<?php echo $row['unique_id'] ?>" class="logout">logout</a>
+     </header>
+     <div class="search">
+       <span class="text">Select an user to start chat</span>
+       <input type="text" placeholder="Enter name to search...">
+       <button><i class="fas fa-search"></i></button>
+     </div>
+     <div class="users-list">
+       
+       
+     </div>
+   </section>
+  </div>
+  <script src="javascript\users.js"></script>
+</body>
+</html>
